0db745420d1cc2b37b2cf582bd0d61744872975a
petrosp <petros@rolling.space>
2019-03-23 19:10:16 +0100

    logs for everyone

--- template.rb
+++ template.rb
@@ -4,8 +4,8 @@ gem 'sqlite3','~> 1.3.6'
 gem 'logstash-event', '~> 1.2.02'
 
 environment 'config.logger = Logger.new(STDOUT)'
-
-environment 'config.lograge.enabled = true', env: :production
-environment 'config.lograge.formatter = Lograge::Formatters::Logstash.new', env: :production
+environment 'config.i18n.fallbacks = true'
+environment 'config.lograge.enabled = true'#, env: :production
+environment 'config.lograge.formatter = Lograge::Formatters::Logstash.new'#, env: :production
 
 environment 'config.web_console.whitelisted_ips = "172.0.0.0/8" if defined?(WebConsole)', env: :development
